f2e99f0159bf41b024b77595c171815b474bc2b4
samour <93775064+samiazt@users.noreply.github.com>
2022-01-17 14:20:25 +0100

    Add files via upload
    Base de données pour la connexion (Clément)

--- clients.sql
+++ clients.sql
@@ -1,30 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : lun. 10 jan. 2022 à 14:51
--- Version du serveur : 10.4.22-MariaDB
--- Version de PHP : 8.1.1
+-- Host: localhost:8889
+-- Generation Time: Jan 17, 2022 at 01:14 PM
+-- Server version: 5.7.32
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Base de données : `espaceclient`
+-- Database: `clients`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `clients`
+-- Table structure for table `clients`
 --
 
 CREATE TABLE `clients` (
@@ -34,11 +27,11 @@ CREATE TABLE `clients` (
   `emailClient` varchar(255) NOT NULL,
   `pseudoClient` varchar(10) NOT NULL,
   `passwordClient` text NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `clients`
+-- Dumping data for table `clients`
 --
 
 INSERT INTO `clients` (`idClient`, `nomClient`, `prenomClient`, `emailClient`, `pseudoClient`, `passwordClient`, `date`) VALUES
@@ -47,11 +40,11 @@ INSERT INTO `clients` (`idClient`, `nomClient`, `prenomClient`, `emailClient`, `
 (3, 'Ruiz', 'Damien', 'damien.ruizzz@gmail.com', 'DMZruiz', 'ceciestunmotdepassse', '2022-01-04 10:15:32');
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `clients`
+-- Indexes for table `clients`
 --
 ALTER TABLE `clients`
   ADD PRIMARY KEY (`idClient`),
@@ -59,16 +52,11 @@ ALTER TABLE `clients`
   ADD UNIQUE KEY `pseudoClient` (`pseudoClient`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `clients`
+-- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
   MODIFY `idClient` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
